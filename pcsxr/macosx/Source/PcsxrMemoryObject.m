cc335091a09bc688edb0ff64b08e6d803b07fb97
MaddTheSane_cp <SND\MaddTheSane_cp>
2015-06-11 03:47:06 +0800

    OS X: faster memory card image generation.

--- pcsxr/macosx/Source/PcsxrMemoryObject.m
+++ pcsxr/macosx/Source/PcsxrMemoryObject.m
@@ -22,32 +22,38 @@
 @property (readwrite) BOOL hasImages;
 @end
 
+#pragma pack(push,2)
+struct PSXRGBColor {
+	UInt8 r;
+	UInt8 g;
+	UInt8 b;
+};
+#pragma pack(pop)
+
 @implementation PcsxrMemoryObject
 
 + (NSArray *)imagesFromMcd:(McdBlock *)block
 {
 	NSMutableArray *imagesArray = [[NSMutableArray alloc] initWithCapacity:block->IconCount];
 	for (int i = 0; i < block->IconCount; i++) {
-		NSImage *memImage;
-		@autoreleasepool {
-			NSBitmapImageRep *imageRep = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:NULL pixelsWide:16 pixelsHigh:16 bitsPerSample:8 samplesPerPixel:3 hasAlpha:NO isPlanar:NO colorSpaceName:NSCalibratedRGBColorSpace bytesPerRow:0 bitsPerPixel:0];
-			
-			short *icon = block->Icon;
-			
-			int x, y, c, v, r, g, b;
-			for (v = 0; v < 256; v++) {
-				x = (v % 16);
-				y = (v / 16);
-				c = icon[(i * 256) + v];
-				r = (c & 0x001f) << 3;
-				g = ((c & 0x03e0) >> 5) << 3;
-				b = ((c & 0x7c00) >> 10) << 3;
-				[imageRep setColor:[NSColor colorWithCalibratedRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1.0] atX:x y:y];
-			}
-			memImage = [[NSImage alloc] init];
-			[memImage addRepresentation:imageRep];
-			[memImage setSize:NSMakeSize(32, 32)];
+		NSBitmapImageRep *imageRep = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:NULL pixelsWide:16 pixelsHigh:16 bitsPerSample:8 samplesPerPixel:3 hasAlpha:NO isPlanar:NO colorSpaceName:NSCalibratedRGBColorSpace bytesPerRow:16*3 bitsPerPixel:24];
+		struct PSXRGBColor *cocoaImageData = (struct PSXRGBColor *)imageRep.bitmapData;
+		short *icon = block->Icon;
+		
+		for (int v = 0; v < 256; v++) {
+			int c = icon[(i * 256) + v];
+			int r = (c & 0x001f) << 3;
+			int g = ((c & 0x03e0) >> 5) << 3;
+			int b = ((c & 0x7c00) >> 10) << 3;
+			struct PSXRGBColor *colorItem = &cocoaImageData[v];
+			//[imageRep setColor:[NSColor colorWithCalibratedRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1.0] atX:x y:y];
+			colorItem->r = r;
+			colorItem->g = g;
+			colorItem->b = b;
 		}
+		NSImage *memImage = [[NSImage alloc] init];
+		[memImage addRepresentation:imageRep];
+		[memImage setSize:NSMakeSize(32, 32)];
 		[imagesArray addObject:memImage];
 	}
 	return [NSArray arrayWithArray:imagesArray];
